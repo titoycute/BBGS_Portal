a3e5f2e41cb5b16acba8dc51abc89bdbeae6f9e9
titoycute <titoycustodio@gmail.com>
2025-09-13 12:51:59 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1627,7 +1627,17 @@ const App = function () {
                             <a href="tel:09666517133" class="text-sm text-pink-400 hover:underline">09666517133</a>
                         </div>
                     </div>
+
                 </div>
+
+                <div style="position: relative; width: 100%; height: 0; padding-top: 100.0000%;
+ padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden;
+ border-radius: 8px; will-change: transform;">
+  <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
+    src="https://www.canva.com/design/DAGyyOxfOro/C22eWaeHJAsyy3SSkP89vA/watch?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+  </iframe>
+</div>
+<a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGyyOxfOro&#x2F;C22eWaeHJAsyy3SSkP89vA&#x2F;watch?utm_content=DAGyyOxfOro&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">What is</a> by Tito Amerigo Custodio Jr.
 
                 <p class="text-center text-xs text-gray-500 pt-4">App Version 2.0.2 (BBGS Pride Pass QR Code System) <br> © 2025 BBGS Dev Tootz </p>
             </div>
